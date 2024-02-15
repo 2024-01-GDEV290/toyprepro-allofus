e87c7f77d9f47b49e7bbe4c6a301a4978b3a409d
Eric Brown <eric.brown8787@gmail.com>
2024-02-15 12:45:24 -0500

    Refactor

--- Assets/Scenes/SceneEric/Crank.cs
+++ Assets/Scenes/SceneEric/Crank.cs
@@ -6,6 +6,11 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum ETimeState{
+    Idle,
+    Winding,
+    ActorMovement
+}
 public class Crank : MonoBehaviour
 {
     [Header("===Set in Inspector===")]
@@ -27,9 +32,11 @@ public class Crank : MonoBehaviour
     [Header("NPC")]
     [SerializeField] Vector3 charStartPos;
     [SerializeField] Vector3 charEndPos;
+    [SerializeField] ETimeState timeState; 
 
     private void Awake()
     {
+        timeState = ETimeState.Idle;
         cylinderRotation = cylinder.transform.localEulerAngles.y;
         dayIntensity = dayLight.intensity;
         nightIntensity = nightLight.intensity;
@@ -43,35 +50,58 @@ public class Crank : MonoBehaviour
         if (Input.GetKey(KeyCode.X))
         {
             RotateClockwise();
+        } else
+        {
+            if(timeState != ETimeState.Idle) timeState = ETimeState.Idle;
         }
         if (Input.GetKey(KeyCode.Z))
         {
             RotateCounterClockwise();
+        }
+        else
+        {
+            if (timeState != ETimeState.Idle) timeState = ETimeState.Idle;
         }
         cylinderRotation = cylinder.transform.localEulerAngles.y;
         if (cylinderRotation < 0)
         {
             cylinder.transform.localEulerAngles = new Vector3(cylinder.transform.localEulerAngles.x, 360 + cylinderRotation, cylinder.transform.localEulerAngles.z);
         }
-        currentAngleDisplay.text = $"Current Rotation: {cylinderRotation}";
+
         // The 360 degrees of the cylinder's rotation = 24 hours
         // 360/24 = 15 degrees per hour
         // 1 min = 15 degrees/60 minutes = .25 degrees
-        float currentHour = Mathf.Floor(cylinderRotation / 15); // Storing time in 24 hour format internally. Will eventually convert to 12 hour format for display, probably
-        float currentMinute = Mathf.Floor((cylinderRotation - currentHour * 15) / .25f);
-        currentTimeDisplay.text = $"Current Time: {currentHour}:{currentMinute}";
+
+
         dayLight.intensity = CalculateArc(cylinderRotation,180)/180 * dayIntensity;
         nightLight.intensity = CalculateArc(cylinderRotation, 0)/180 * nightIntensity;
         npc.transform.position = Vector3.Lerp(charStartPos, charEndPos, CalculateArc(cylinderRotation, 180)/180);
+        UpdateUI();
     }
+
+    string GetCurrentTimeString()
+    {
+        float currentHour = Mathf.Floor(cylinderRotation / 15);
+        float currentMinute = Mathf.Floor((cylinderRotation - currentHour * 15) / .25f);
+        return $"{currentHour}:{currentMinute}";
+    }
+
+    void UpdateUI()
+    {
+        currentAngleDisplay.text = $"Current Rotation: {cylinderRotation}";
+        currentTimeDisplay.text = $"Current Time: {GetCurrentTimeString()}";
+    }
+
 
     void RotateClockwise()
     {
+        if (timeState != ETimeState.Winding) timeState = ETimeState.Winding;
         cylinder.transform.localEulerAngles = new Vector3(cylinder.transform.localEulerAngles.x, cylinderRotation + timeFlowRate * Time.deltaTime,cylinder.transform.localEulerAngles.z);
     }
 
     void RotateCounterClockwise()
     {
+        if (timeState != ETimeState.Winding) timeState = ETimeState.Winding;
         cylinder.transform.localEulerAngles = new Vector3(cylinder.transform.localEulerAngles.x, cylinderRotation - timeFlowRate * Time.deltaTime, cylinder.transform.localEulerAngles.z);
     }
     float CalculateArc(float currentAngle,float brightestAngle)
