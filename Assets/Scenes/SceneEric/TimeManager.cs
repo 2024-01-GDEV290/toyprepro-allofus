b9ac74c9b971e6c8d6b723522efee814defcd986
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2024-02-14 21:10:20 -0500

    Remove unused scripts

--- Assets/Scenes/SceneEric/TimeManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu]
-public class TimeManager : ScriptableObject
-{
-    public float currentTime = 0; 
-}
