4f44d57c84b2c92dac3500af8a757c644a687713
projectgild <137822248+projectgild@users.noreply.github.com>
2024-02-18 18:59:49 -0500

    Added test model with movable arms

--- Assets/Scripts/playerInput.cs
+++ Assets/Scripts/playerInput.cs
@@ -8,22 +8,29 @@ public class playerInput : MonoBehaviour
 	// float values for joystick and trigger input from Xbox controller
     float ls_vertical = 0;
     float ls_horizontal = 0;
+	
 	float rs_vertical = 0;
     float rs_horizontal = 0;
+	
 	float triggers = 0;
 	
-	// Both input axis thresholds for the vertical and horizontal axis
+	// Both joystick input axis thresholds for the vertical and horizontal axis
     // are between -1 and 0.
     double lowerThreshold = -1;
     double upperThreshold = -0.2;
 	
 	// values used for translating input into gameplay actions
-    bool lh_swimming;
-	bool rh_swimming;
 	
 	float lh_swimming_intensity = 0;
 	float rh_swimming_intensity = 0;
-
+	
+	// GameObject references for arm movement
+	public GameObject rightShoulder;
+	public GameObject rightElbow;
+	
+	public GameObject leftShoulder;
+	public GameObject leftElbow;
+	
     // Start is called before the first frame update
     void Start()
     {
@@ -37,14 +44,12 @@ public class playerInput : MonoBehaviour
         ls_vertical = Input.GetAxis("Vertical");
         ls_horizontal = Input.GetAxis("Horizontal");
 		
-		lh_swimming = false;
 		lh_swimming_intensity = 0;
 
         if (ls_vertical >= lowerThreshold && ls_vertical <= upperThreshold)
         {
             if (ls_horizontal >= lowerThreshold && ls_horizontal <= upperThreshold)
             {
-                lh_swimming = true;
 				lh_swimming_intensity = ls_vertical * ls_horizontal;
 				Debug.Log("Left-Hand Swim Intensity: " + lh_swimming_intensity);
             }
@@ -54,11 +59,12 @@ public class playerInput : MonoBehaviour
         rs_vertical = Input.GetAxis("rs_vertical") * -1;
         rs_horizontal = Input.GetAxis("rs_horizontal") * -1;
 		
+		rh_swimming_intensity = 0;
+		
 		if (rs_vertical >= lowerThreshold && rs_vertical <= upperThreshold)
         {
             if (rs_horizontal >= lowerThreshold && rs_horizontal <= upperThreshold)
             {
-                rh_swimming = true;
 				rh_swimming_intensity = rs_vertical * rs_horizontal;
 				Debug.Log("Right-Hand Swim Intensity: " + rh_swimming_intensity);
             }
@@ -74,5 +80,48 @@ public class playerInput : MonoBehaviour
 		{
 			Debug.Log("Right Foot Intensity: " + triggers);
 		}
+		
+		
+		// Test animations for arms
+		moveRightArm(rightShoulder, rightElbow, rh_swimming_intensity);
+		moveLeftArm(leftShoulder, leftElbow, lh_swimming_intensity);
     }
+	
+	void moveRightArm(GameObject shoulder, GameObject elbow, float intensity)
+	{
+		float shoulder_degrees;
+		float elbow_degrees;
+		
+		if (intensity != 0)
+		{
+			shoulder_degrees = (intensity * 120) + 30;
+			elbow_degrees = (intensity * 120) - 60;
+		} else
+		{
+			shoulder_degrees = 30;
+			elbow_degrees = -60;
+		}
+		
+		shoulder.transform.localRotation = Quaternion.Euler(-136.21F, 0, shoulder_degrees);
+		elbow.transform.localRotation = Quaternion.Euler(0, 0, elbow_degrees);
+	}
+	
+	void moveLeftArm(GameObject shoulder, GameObject elbow, float intensity)
+	{
+		float shoulder_degrees;
+		float elbow_degrees;
+		
+		if (intensity != 0)
+		{
+			shoulder_degrees = (intensity * 120) + 30;
+			elbow_degrees = (intensity * 120) - 60;
+		} else
+		{
+			shoulder_degrees = 30;
+			elbow_degrees = -60;
+		}
+		
+		shoulder.transform.localRotation = Quaternion.Euler(-226.21F, 180, shoulder_degrees);
+		elbow.transform.localRotation = Quaternion.Euler(0, 0, elbow_degrees);
+	}
 }
