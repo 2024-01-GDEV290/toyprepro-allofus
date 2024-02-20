73cb9ca5134fdc025ad7a42e315353521bc97ff9
MegaMagicarp <66321765+MegaMagicarp@users.noreply.github.com>
2024-02-20 18:54:54 -0500

    battery is collectable

--- Assets/Recharge/__Scripts/BatteryGet.cs
+++ Assets/Recharge/__Scripts/BatteryGet.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BatteryGet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Text powerText;
+    private int powerRemaining = 100;
+
+    // OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider.
+    private void OnTriggerEnter(Collider other)
     {
+        // Delete the colliding object
+        Destroy(other.gameObject);
         
+        // Update the UI text
+        UpdatePowerText();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdatePowerText()
     {
-        
+        // Update the UI text with the remaining power value
+        if (powerText != null)
+        {
+            powerText.text = "Power Remaining: " + powerRemaining;
+        }
+    }
+
+    private void Start()
+    {
+        // Set up the initial UI text
+        UpdatePowerText();
     }
 }
---
+++ Assets/Recharge/__Scripts/SimplePlayerController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerController : MonoBehaviour
+{
+    public float movementSpeed = 5f;
+    public float mouseSensitivity = 2f;
+    public float jumpForce = 10f;
+
+    private float verticalRotation = 0f;
+    private bool isGrounded;
+
+    private void Update()
+    {
+        // Handle player movement
+        MovePlayer();
+
+        // Handle player rotation (looking around)
+        RotatePlayer();
+
+        // Handle player jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void MovePlayer()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical) * movementSpeed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+
+    void RotatePlayer()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        verticalRotation -= mouseY * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+
+        transform.Rotate(Vector3.up * mouseX * mouseSensitivity);
+        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the player is grounded (touching the ground)
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
