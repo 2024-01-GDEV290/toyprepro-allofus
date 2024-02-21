f6ce9399312cd744755afcc3ff0a645e8aba9091
kfederici <143634514+kfederici@users.noreply.github.com>
2024-02-21 10:04:33 -0500

    third person camera set up

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -2,37 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Camera))]
+[RequireComponent(typeof(CharacterController))]
 public class ThirdPersonMovement : MonoBehaviour
 {
-    public float moveSpeed;
-    public float shiftAdditionalSpeed;
-    public float mouseSensitivity;public bool invertMouse;
-    public bool autoLockCursor;
-    private Camera cam;
+    public float Speed = 7.5f;
+    public Transform playerCameraParent;
+    public float lookSpeed = 2f;
+    public float lookXLimit = 60f;
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    Vector2 rotation = Vector2.zero;
 
-    void Awake()
+    [HideInInspector]
+    public bool canMove = true;
+
+    private void Start()
     {
-        cam = this.gameObject.GetComponent<Camera>();
-        this.gameObject.name = "SpectatorCamera";
-        Cursor.lockState = (autoLockCursor)?CursorLockMode.Locked:CursorLockMode.None;
+        characterController = GetComponent<CharacterController>();
+        rotation.y = transform.eulerAngles.y;
     }
+
+    
 
     void Update()
     {
-        float speed = (moveSpeed + (Input.GetAxis("Fire3") * shiftAdditionalSpeed));
-        this.gameObject.transform.Translate(Vector3.forward * speed * Input.GetAxis("Vertical"));
-        this.gameObject.transform.Translate(Vector3.right * speed * Input.GetAxis("Horizontal"));
-        this.gameObject.transform.Translate(Vector3.up * speed * (Input.GetAxis("Jump") + (Input.GetAxis("Fire1") * -1)));
-        this.gameObject.transform.Rotate(Input.GetAxis("Mouse Y") * mouseSensitivity * ((invertMouse) ? 1 : -1), Input.GetAxis("Mouse x") * ((invertMouse) ? -1 : 1), 0);
-        this.gameObject.transform.localEulerAngles = new Vector3(this.gameObject.transform.localEulerAngles.x, this.gameObject.transform.localEulerAngles.y, 0);
-        if (Cursor.lockState == CursorLockMode.None && Input.GetMouseButtonDown(0))
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        float curSpeedX = canMove ? Speed * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? Speed * Input.GetAxis("Horizontal") : 0;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (canMove)
         {
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-        else if (Cursor.lockState == CursorLockMode.Locked && Input.GetKeyDown(KeyCode.Escape))
-        {
-            Cursor.lockState = CursorLockMode.None;
+            rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
+            rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
+            playerCameraParent.localRotation = Quaternion.Euler(rotation.x, 0, 0);
+            transform.eulerAngles = new Vector2(0, rotation.y);
         }
     }
+
 }
