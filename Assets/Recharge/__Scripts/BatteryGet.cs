614d5cec1d006dd5e045b881dcd1c34025c8dcfb
MegaMagicarp <66321765+MegaMagicarp@users.noreply.github.com>
2024-02-20 21:18:58 -0500

    Update BatteryGet.cs

--- Assets/Recharge/__Scripts/BatteryGet.cs
+++ Assets/Recharge/__Scripts/BatteryGet.cs
@@ -7,13 +7,18 @@ public class BatteryGet : MonoBehaviour
 {
     public TMP_Text powerText;
     public int powerRemaining = 100;
+    private float timer = 0.5f;
 
     // OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider.
     private void OnTriggerEnter(Collider other)
     {
         // Delete the colliding object
         Destroy(other.gameObject);
-        
+        powerRemaining += 25;
+        if (powerRemaining > 100)
+        {
+            powerRemaining = 100;
+        }
         // Update the UI text
         UpdatePowerText();
     }
@@ -21,6 +26,7 @@ public class BatteryGet : MonoBehaviour
     private void UpdatePowerText()
     {
         // Update the UI text with the remaining power value
+        powerText.text = powerText.text;
         powerText.text = powerText.text + powerRemaining;
     }
 
@@ -34,15 +40,17 @@ public class BatteryGet : MonoBehaviour
 
     private void DrainPower()
     {
-        for (int i = 100; i >= powerRemaining && i != 0;)
-        {
-            i--;
-            powerRemaining = i;
-        }
+        powerRemaining--;
+        UpdatePowerText();
     }
 
     private void Update()
     {
-        UpdatePowerText();
+        timer -= Time.deltaTime;
+        if (timer <= 0.0f)
+        {
+            DrainPower();
+            timer = .5f;
+        }
     }
 }
