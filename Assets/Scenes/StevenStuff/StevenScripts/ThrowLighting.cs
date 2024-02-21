9fb8169cbf3d57d78a7bd39091369e1ce1141e4a
StevenC-623 <130469793+StevenC-623@users.noreply.github.com>
2024-02-20 20:09:05 -0500

    zap

--- Assets/Scenes/StevenStuff/StevenScripts/ThrowLighting.cs
+++ Assets/Scenes/StevenStuff/StevenScripts/ThrowLighting.cs
@@ -1,18 +1,41 @@
+using DigitalRuby.LightningBolt;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ThrowLighting : MonoBehaviour
 {
+    [SerializeField] private GameObject bolt1;
+    [SerializeField] private GameObject bolt2;
+    [SerializeField] private GameObject bolt3;
+    [SerializeField] private GameObject bolt4;
+    private Boolean shocking = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //left click
+        if (Input.GetKeyDown("mouse 0"))
+        {
+            bolt1.SetActive(true);
+            bolt2.SetActive(true);
+            bolt3.SetActive(true);
+            bolt4.SetActive(true);
+           
+        }
+        else if (Input.GetKeyUp("mouse 0"))
+        {
+            bolt1.SetActive(false);
+            bolt2.SetActive(false);
+            bolt3.SetActive(false);
+            bolt4.SetActive(false);
+
+        }
     }
 }
