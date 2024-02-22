e2f1802c3287b27f5ea452ef4c94b89edc72db31
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2024-02-21 22:05:03 -0500

    Add interaction description zip and improve visuals

--- Assets/Scripts/Sonar.cs
+++ Assets/Scripts/Sonar.cs
@@ -6,9 +6,19 @@ public class Sonar : MonoBehaviour
 {
     [SerializeField] GameObject sweep;
     [SerializeField] float rotationSpeed = 1;
+    AudioSource playerAudioSource;
+    [SerializeField] AudioClip sonarSound;
 
+    private void Awake()
+    {
+         playerAudioSource = transform.parent.gameObject.GetComponent<AudioSource>();
+    }
     void Update()
     {
+        if (!playerAudioSource.isPlaying)
+        {
+            playerAudioSource.PlayOneShot(sonarSound);
+        } 
         transform.eulerAngles += new Vector3(0, rotationSpeed * Time.deltaTime, 0);
     }
 }
