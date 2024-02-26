7203d3cb8b8f182ee0b7b427c5f551dc661bb2ab
Jammerjab1 <89883568+Jammerjab1@users.noreply.github.com>
2024-02-26 17:27:48 -0500

    Implemented Dry Timer

--- Assets/Scenes/Soak/___Scripts/Soak.cs
+++ Assets/Scenes/Soak/___Scripts/Soak.cs
@@ -8,16 +8,36 @@ public class Soak : MonoBehaviour
     public Animator animator;
     public ParticleSystem splash;
 
+    bool isWet = false;
+    int dryTimer = 200;
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (isWet == true)
+        {
+            dryTimer -= 1;
+        }
+
+        if (dryTimer <= 15) // Play animation before timer resets
+        {
+            animator.SetBool("Soaked", false);
+        }
+
+        if (dryTimer <= 0)
+        {
+            isWet = false;
+            dryTimer = 200;
+        }
     }
 
     private void OnParticleCollision(GameObject other)
     {   
-
-        animator.SetBool("Soaked", true);
-        splash.Play();
+        if (isWet == false)
+        {
+            animator.SetBool("Soaked", true);
+            splash.Play();
+            isWet = true;
+        }
     }
 }
