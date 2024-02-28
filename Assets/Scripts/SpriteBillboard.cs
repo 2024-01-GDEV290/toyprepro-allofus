fa65d646e5839c7caa3413d24b1c0bb7ee4fda1e
Eric Brown <90017825+ericbrown8787@users.noreply.github.com>
2024-02-27 20:42:51 -0500

    More experiments

--- Assets/Scripts/SpriteBillboard.cs
+++ Assets/Scripts/SpriteBillboard.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.SearchService;
+using UnityEditor.ShaderGraph.Drawing;
 using UnityEngine;
 
 public class SpriteBillboard : MonoBehaviour
 {
     [SerializeField] bool freezeXZAxis = true;
+    [SerializeField] Sprite front;
+    [SerializeField] Sprite back;
+    [SerializeField] Sprite left;
+    [SerializeField] Sprite right;
     SpriteRenderer spriteRenderer;
     Transform parent;
 
     private void Awake()
     {
+
         parent = transform.parent;
         spriteRenderer = GetComponent<SpriteRenderer>();
-        Debug.Log(spriteRenderer.sprite);
     }
     void LateUpdate()
     {
@@ -25,9 +30,54 @@ public class SpriteBillboard : MonoBehaviour
         {
             transform.rotation = Camera.main.transform.rotation;
         }
-        GameObject player = GameObject.Find("Player");
-        Vector2 mainCam2DPos = new Vector2(Camera.main.transform.position.x,Camera.main.transform.position.z); 
-        Vector2 parentForward = new Vector2(parent.transform.forward.x,parent.transform.forward.z);
-       /* Debug.Log(Vector2.Angle(mainCam2DPos,parentForward));*/
+        GameObject player = GameObject.Find("Player"); 
+        Vector3 parentForward = parent.transform.forward;
+        Vector3 parentLeft = Quaternion.Euler(0, 90, 0) * parent.transform.forward;
+        float northSouth = Vector3.Angle(Camera.main.transform.position,parentForward);
+        float eastWest = Vector3.Angle(parentLeft, Camera.main.transform.position);
+        Debug.DrawRay(transform.position, parentLeft, Color.red) ;
+        Debug.DrawRay(transform.position, parentForward, Color.blue);
+        Debug.Log($"Deg from sprite forward: {northSouth}");
+        Debug.Log($"Deg from sprite right: {eastWest}");
+        Sprite newSprite = spriteRenderer.sprite;
+        if (northSouth > 90)
+        {
+            if (eastWest < 45)
+            {
+                newSprite = right;
+            } else if (eastWest > 135)
+            {
+                newSprite = left;
+            }else
+            {
+                newSprite = back;
+            }
+        }
+        else if (northSouth < 90)
+        {
+            if (eastWest < 45)
+            {
+                newSprite = right;
+            }
+            else if (eastWest > 135)
+            {
+                newSprite = left;
+            }
+            else
+            {
+                newSprite = front;
+            }
+        }
+        spriteRenderer.sprite = newSprite;
+        /*        if (northSouth < 90)
+                {
+                    spriteRenderer.sprite = front;
+                }
+                else
+                {
+                    spriteRenderer.sprite = back;
+                }*/
+        /* Debug.Log(Vector2.Angle(mainCam2DPos,parentForward));*/
+
     }
 }
