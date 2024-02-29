4d3b0f048aec4961004bc22d12d5dceef5c27843
KM-08092021 <90330252+KM-08092021@users.noreply.github.com>
2024-02-28 20:53:16 -0500

    Currently making new version with connectable gates

--- Assets/Scripts/Shocker.cs
+++ Assets/Scripts/Shocker.cs
@@ -6,9 +6,7 @@ using TMPro;
 public class Shocker : MonoBehaviour
 {
 	//shocker_intensity is the raw 0 to 1 float input from the trigger.
-	//shocker_power is that number, translated into int values between 0 and 3.
 	float shocker_intensity;
-	int shocker_power;
 	
 	//The following three vars are used for identification of the shocker's power level.
 	[SerializeField]
@@ -17,12 +15,15 @@ public class Shocker : MonoBehaviour
 	[SerializeField]
 	private AudioSource intensity_audio;
 
-	[SerializeField]
-	private TMP_Text intensity_meter;
+	RaycastHit sro_data;
+	RaycastHit src_data;
 	
-	//Raycast used for shocking
-	Ray shocker_ray;
-	RaycastHit shocker_data;
+	[SerializeField]
+	private GameObject ray_end;
+	
+	
+	private bool is_shocking = false;
+	private bool closed_interact = false;
 	
     // Start is called before the first frame update
     void Start()
@@ -39,62 +40,51 @@ public class Shocker : MonoBehaviour
 		intensity_light.intensity = shocker_intensity;
 		intensity_audio.volume = shocker_intensity;
 		intensity_audio.pitch = shocker_intensity/2 + 0.5F;
-
-		//calculates shocker power level, also shows on intensity meter
-		shocker_power = calculate_power(shocker_intensity);
-		intensity_meter.text = (shocker_power.ToString());
 		
-		if (Input.GetButtonDown("Shoot Shocker"))
+		
+		Physics.Raycast(transform.position, transform.forward, out sro_data, 10);
+		Physics.Raycast(transform.position, transform.forward, out src_data, 10);
+			
+		
+		if (shocker_intensity != 0)
 		{
-			shock(shocker_power);
+			shock_connect(sro_data);
 		}
+		
+		if (shocker_intensity == 0)
+		{
+			is_shocking = false;
+		}
+		
+		
+		if(sro_data.collider.tag == "closed")
+		{
+			closed_interact = true;
+		} else {
+			closed_interact = false;
+		}
+		
+		Debug.Log(closed_interact);
     }
 	
-	int calculate_power(float intensity)
+	void shock_connect(RaycastHit data)
 	{
 		
-		return (int)(shocker_intensity * 3);
-	}
-	
-	void shock(int power)
-	{
-		shocker_ray = new Ray(transform.position, transform.forward);
-		Physics.Raycast(shocker_ray, out shocker_data);
 		
-		switch(shocker_data.collider.tag)
+		if (data.collider.tag == "open")
 		{
-			case "level1":
-				if (power == 1)
-				{
-					Debug.Log("Shocked circuit!!");
-				} else {
-					Debug.Log("Missed...");
-				}
-				break;
-			case "level2":
-				if (power == 2)
-				{
-					Debug.Log("Shocked circuit!!");
-				} else {
-					Debug.Log("Missed...");
-				}
-				break;
-			case "level3":
-				if (power == 3)
-				{
-					Debug.Log("Shocked circuit!!");
-				} else {
-					Debug.Log("Missed...");
-				}
-				break;
-			case null:
-				Debug.Log("Didn't hit anything...");
-				break;
-			default:
-				Debug.Log("Not a circuit...");
-				break;
+			is_shocking = true;
 		}
-				
 		
+		if (is_shocking == true)
+		{
+			Debug.Log("Searching...");
+		}
+		
+		
+		if (is_shocking == true && closed_interact == true)
+		{
+			Debug.Log("Shocking!! SHOCKING!!!!");
+		}
 	}
 }
