5019a3bd52aba7e32e2e269947cb37ae9c0aefd0
ArtemisMacDuffie <G00168172@stu.raritanval.edu>
2024-03-13 23:26:11 -0400

    apple spawn, ripen, drop

--- Assets/Scripts/Items/AppleSpawn.cs
+++ Assets/Scripts/Items/AppleSpawn.cs
@@ -6,5 +6,15 @@ public class AppleSpawn : MonoBehaviour
 {
     public GameObject applePrefab;
 
-
+    public void MakeApple()
+    {
+        if (WindingTime.S.hours == 2)
+        {
+            GameObject newApple = Instantiate(applePrefab);
+            newApple.transform.SetParent(this.transform);
+            newApple.transform.position = Vector3.zero;
+            newApple.GetComponent<AppleTime>().animating = true;
+            newApple.GetComponent<AppleTime>().onTree = true;
+        }
+    }
 }
--- Assets/Scripts/Items/AppleTime.cs
+++ Assets/Scripts/Items/AppleTime.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AppleTime : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator anim;
+
+    [Header("Set dynamically")]
+    public bool animating;
+    public bool onTree;
+    public float ripeness;
+    
+    private void Awake()
     {
-        
+        anim = GetComponent<Animator>();
+        ripeness = 0;
+        animating = false;
+        onTree = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AppleAnim()
     {
-        
+        if (!animating) { return; }
+
+        ripeness = (WindingTime.S.hours - 2f) / 20f;
+
+        if (ripeness >= 1) { Destroy(this.gameObject); }
+        else
+        {
+            anim.Play("AppleAnim", 0, ripeness);
+            if (ripeness >= 0.4) { onTree = false; }
+        }
     }
 }
